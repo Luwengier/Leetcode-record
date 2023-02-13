5f29158e1b33012347f011a7402f55acbf94ca0d
Luewngier <g217881111@gmail.com>
2023-02-13 23:16:47 +0800

    Add 9. Palindrome Number

--- 9.PalindromeNumber.js
+++ 9.PalindromeNumber.js
@@ -6,6 +6,8 @@
     return str.split("").reverse().join("");
 }
 
+//
+
 var isPalindrome = function(x) {
     const l = x.toString().length / 2
     return x.toString().slice(0, Math.ceil(l)) === reverseString(x.toString().slice(Math.floor(l)))
