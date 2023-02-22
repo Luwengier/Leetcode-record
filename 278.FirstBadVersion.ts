2e7133c72f87f7b6df65e265c6d81d288baf7b1a
Luwengier <g217881111@gmail.com>
2023-02-23 02:26:47 +0800

    Add 278. First Bad Version

---
+++ 278.FirstBadVersion.ts
@@ -0,0 +1,26 @@
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function (isBadVersion: any) {
+  return function (n: number): number {
+    if (isBadVersion(1)) return 1
+    let l = 1
+    let r = n
+    let cur: number
+
+    while (l !== r) {
+      cur = Math.floor((l + r) / 2)
+      if (isBadVersion(cur)) {
+        r = cur
+      } else {
+        l = cur + 1
+      }
+    }
+
+    return l
+  }
+}
