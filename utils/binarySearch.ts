a92b32972f6c2cb4a36b4430f52b59583a080d8e
Luwengier <g217881111@gmail.com>
2023-02-23 02:27:13 +0800

    Add 704. Binary Search

---
+++ utils/binarySearch.ts
@@ -0,0 +1,19 @@
+function search(nums: number[], target: number): number {
+  let l = 0
+  let r = nums.length - 1
+  let cur: number
+
+  while (l !== r) {
+    cur = Math.floor((l + r) / 2)
+
+    if (nums[cur] === target) {
+      return cur
+    } else if (nums[cur] > target) {
+      r = cur
+    } else {
+      l = cur + 1
+    }
+  }
+
+  return nums[l] === target ? l : -1
+}
