5f85b1a9ce769dac059b5e62aa66faefb670ba08
Luewngier <g217881111@gmail.com>
2023-05-16 19:40:56 +0800

    Add 26. Remove Duplicates from Sorted Array

---
+++ 26.RemoveDuplicatesFromSortedArray.ts
@@ -0,0 +1,24 @@
+function removeDuplicates(nums: number[]): number {
+  let index = 0;
+  let counter = 0;
+  const originLength = nums.length;
+
+  while (index < nums.length) {
+    while (
+      nums[index + counter] === nums[index + counter + 1] &&
+      nums[index + counter] !== undefined
+    ) {
+      counter++;
+    }
+    if (counter) {
+      nums.splice(index + 1, counter);
+      counter = 0;
+    }
+    index++;
+  }
+
+  const k = nums.length;
+  nums.splice(k, 0, ...new Array(originLength - k));
+
+  return k;
+}
