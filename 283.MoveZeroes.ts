ff834449e750a5ac6d3f72af4583e68e80e3fa27
Luewngier <g217881111@gmail.com>
2023-06-24 21:28:34 +0800

    Update 283.

--- 283.MoveZeroes.ts
+++ 283.MoveZeroes.ts
@@ -1,24 +1,37 @@
 /**
  Do not return anything, modify nums in-place instead.
  */
+
 function moveZeroes(nums: number[]): void {
-  let acc = 0
-  let startIndex = 0
-  const initialLen = nums.length
-
-  for (let i = 0; i < nums.length; ) {
-    if (nums[i] === 0) {
-      acc = 1
-      startIndex = i
-
-      while (acc[i + 1] === 0) {
-        acc++
-        i++
-      }
-
-      nums.splice(startIndex, acc)
-    } else i++
+  let left = 0
+  for (let right = 0; right < nums.length; right++) {
+    const val = nums[right]
+    nums[right] = 0
+    if (val !== 0) {
+      nums[left] = val
+      left++
+    }
   }
-
-  while (initialLen > nums.length) nums.push(0)
 }
+
+// function moveZeroes(nums: number[]): void {
+//   let acc = 0
+//   let startIndex = 0
+//   const initialLen = nums.length
+
+//   for (let i = 0; i < nums.length; ) {
+//     if (nums[i] === 0) {
+//       acc = 1
+//       startIndex = i
+
+//       while (acc[i + 1] === 0) {
+//         acc++
+//         i++
+//       }
+
+//       nums.splice(startIndex, acc)
+//     } else i++
+//   }
+
+//   while (initialLen > nums.length) nums.push(0)
+// }
