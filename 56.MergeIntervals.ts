42543eefbbd56c955567b95268c67bde3eaf4199
Luewngier <g217881111@gmail.com>
2023-07-19 17:25:31 +0800

    Add 56. Merge Intervals

---
+++ 56.MergeIntervals.ts
@@ -0,0 +1,17 @@
+function merge(intervals: number[][]): number[][] {
+  intervals.sort((a, b) => a[0] - b[0])
+  let i = 0
+
+  while (i < intervals.length - 1) {
+    if (intervals[i][1] >= intervals[i + 1][0]) {
+      intervals.splice(i, 2, [
+        intervals[i][0],
+        Math.max(intervals[i][1], intervals[i + 1][1]),
+      ])
+    } else {
+      i++
+    }
+  }
+
+  return intervals
+}
